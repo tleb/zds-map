a034624939f8039b18fe873d4a02a5871253eb13
tleb <tleb@openmailbox.org>
2017-07-20 17:28:42 +0200

    Progress is not being made

--- zds.py
+++ zds.py
@@ -12,11 +12,25 @@ class Zds:
         self._refresh_token = None
         self.access_token = ''
 
-    def _request(self, uri_path, method='GET'):
+    def _request(self, uri_path, method='GET', body=None, returned_unauthorized=False):
+        # TODO: add infos to the errors' messages
         if not self.access_token:
             self._refresh_access_token()
 
-        pass
+        uri = self.base_uri + uri_path
+        headers = {'Authorization': 'Bearer {}'.format(self.access_token)}
+
+        res = requests.request(method, uri, headers=headers, json=body)
+
+        if res.status_code == 401:
+            if returned_unauthorized:
+                raise RuntimeError('Two 401 in a row')
+            self._refresh_access_token()
+            return self._request(uri_path, method, body, True)
+        elif res.status_code != 200:
+            raise RuntimeError('ZdS answered with an unexpected status code')
+
+        return res
 
     def _refresh_access_token(self):
         uri = '{}/oauth2/token/'.format(self.base_uri)
@@ -35,7 +49,7 @@ class Zds:
     def refresh_token(self):
         if self._refresh_token is None:
             try:
-                with open(self.refresh_token_path, 'UTF-8') as f:
+                with open(self.refresh_token_path, encoding='UTF-8') as f:
                     self._refresh_token = f.read().strip()
             except FileNotFoundError:
                 self._refresh_token = ''
@@ -44,7 +58,7 @@ class Zds:
 
     @refresh_token.setter
     def refresh_token(self, token):
-        with open(self.refresh_token_path, 'UTF-8') as f:
+        with open(self.refresh_token_path, encoding='UTF-8') as f:
             f.write(token)
 
         self._refresh_token = token
